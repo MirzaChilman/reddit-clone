7245f575ee30fc987aeebc8a82d9337cfc3b41f0
Mirza Chilman <mirzachilman@gmail.com>
2023-11-15 21:59:05 +0700

    fix: vote breaks on change type

--- components/(client)/Votes/Votes.jsx
+++ components/(client)/Votes/Votes.jsx
@@ -3,11 +3,31 @@ import { ArrowBigUp, ArrowBigDown } from "lucide-react";
 import { patchPost } from "@/app/actions/patchPost";
 import { useParams, useRouter } from "next/navigation";
 import { useCookies } from "next-client-cookies";
+import { useToast } from "@/components/ui/use-toast";
+import { useState } from "react";
 
 export const VotesAction = ({ id, votes }) => {
+  const { toast } = useToast();
+  const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
   const cookieStore = useCookies();
   const params = useParams();
+
+  const handleVoteClick = async (votes) => {
+    setIsLoading(true);
+    try {
+      await patchPost({ id, votes: votes });
+      router.refresh();
+    } catch (error) {
+      toast({
+        title: "Fail to update vote",
+        variant: "destructive",
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <section
       className={`${
@@ -18,19 +38,23 @@ export const VotesAction = ({ id, votes }) => {
     >
       <ArrowBigUp
         color="gray"
-        className="cursor-pointer hover:bg-secondary"
-        onClick={async () => {
-          await patchPost({ id, votes: votes + 1 });
-          router.refresh();
+        className={`cursor-pointer hover:bg-secondary ${
+          isLoading ? "opacity-50" : ""
+        }`}
+        disabled={isLoading}
+        onClick={() => {
+          !isLoading && handleVoteClick(votes + 1);
         }}
       />
       <p>{votes}</p>
       <ArrowBigDown
         color="gray"
-        className="cursor-pointer hover:bg-secondary"
-        onClick={async () => {
-          await patchPost({ id, votes: votes - 1 });
-          router.refresh();
+        className={`cursor-pointer hover:bg-secondary ${
+          isLoading ? "opacity-50" : ""
+        }`}
+        disabled={isLoading}
+        onClick={() => {
+          !isLoading && handleVoteClick(votes - 1);
         }}
       />
     </section>
