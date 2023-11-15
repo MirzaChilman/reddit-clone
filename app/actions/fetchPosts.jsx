5c2c1d8ba6f1ea0607b8e0daaa6cd7fb4cef4229
Mirza Chilman <mirzachilman@gmail.com>
2023-11-15 15:48:49 +0700

    feat: give limit on fetch posts

--- app/actions/fetchPosts.jsx
+++ app/actions/fetchPosts.jsx
@@ -1,7 +1,7 @@
 "use server";
 export async function fetchPosts({ type = "best", page = 0 } = {}) {
   const res = await fetch(
-    `${process.env.BASE_URL}/posts?type=${type}&_limit=&_page=${page}`
+    `${process.env.BASE_URL}/posts?type=${type}&_limit=5&_page=${page}`
   );
   const data = await res.json();
   return { data };
